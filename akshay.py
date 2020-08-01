66cb2d4cba14070809788603a5da9c9ac9d35ea8
while-true-py <69073730+while-true-py@users.noreply.github.com>
2020-08-01 14:41:54 +0530

    Add files via upload

---
+++ akshay.py
@@ -0,0 +1,14 @@
+while True:
+    a = int(input("Enter your number"))
+    b = int(input("Enter new number"))
+
+    print('''
+    a+b,
+    a-b,
+    a*b,
+    a/b''')
+    print("Your ans. is ready.")
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
